41ad180ed7429d22d7917cf579efa391e1b37923
rishel <rishelarora@gmail.com]>
2016-01-21 11:28:49 +0530

    1st commit

--- app/src/main/java/com/example/rishel/ocr/OcrActivity.java
+++ app/src/main/java/com/example/rishel/ocr/OcrActivity.java
@@ -1,6 +1,7 @@
 package com.example.rishel.ocr;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
@@ -8,6 +9,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -181,7 +183,7 @@ public class OcrActivity extends Activity {
         Log.i(TAG, "resultCode: " + resultCode);
 
         if (resultCode == -1) {
-            onPhotoTaken();
+            new OCRTask().execute();
         } else {
             Log.v(TAG, "User cancelled");
         }
@@ -196,100 +198,99 @@ public class OcrActivity extends Activity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         Log.i(TAG, "onRestoreInstanceState()");
         if (savedInstanceState.getBoolean(OcrActivity.PHOTO_TAKEN)) {
-            onPhotoTaken();
-
+            new OCRTask().execute();
         }
     }
 
 
-    protected void onPhotoTaken() {
-        _taken = true;
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
-
-        try {
-            ExifInterface exif = new ExifInterface(_path);
-            int exifOrientation = exif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION,
-                    ExifInterface.ORIENTATION_NORMAL);
-
-            Log.v(TAG, "Orient: " + exifOrientation);
-
-            int rotate = 0;
-
-            switch (exifOrientation) {
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotate = 90;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotate = 180;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotate = 270;
-                    break;
-            }
-
-            Log.v(TAG, "Rotation: " + rotate);
-
-            if (rotate != 0) {
-
-                // Getting width & height of the given image.
-                int w = bitmap.getWidth();
-                int h = bitmap.getHeight();
-
-                // Setting pre rotate
-                Matrix mtx = new Matrix();
-                mtx.preRotate(rotate);
-
-                // Rotating Bitmap
-                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-            }
-
-            // Convert to ARGB_8888, required by tess
-            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-        } catch (IOException e) {
-            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
-        }
-
-        // _image.setImageBitmap( bitmap );
-
-        Log.v(TAG, "Before baseApi");
-
-        TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.setDebug(true);
-        baseApi.init(DATA_PATH, lang);
-        baseApi.setImage(bitmap);
-
-        String recognizedText = baseApi.getUTF8Text();
-
-        baseApi.end();
-
-        // You now have the text in recognizedText var, you can do anything with it.
-        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-        // so that garbage doesn't make it to the display.
-
-        Log.v(TAG, "OCRED TEXT: " + recognizedText);
-
-        if ( lang.equalsIgnoreCase("eng") ) {
-            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
-        }
-
-        recognizedText = recognizedText.trim();
-
-        if ( recognizedText.length() != 0 ) {
-            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
-            _field.setSelection(_field.getText().toString().length());
-
-        }
-
-
-
-        // Cycle done.
-    }
+//    protected void onPhotoTaken() {
+//        _taken = true;
+//
+//        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inSampleSize = 4;
+//
+//        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+//
+//        try {
+//            ExifInterface exif = new ExifInterface(_path);
+//            int exifOrientation = exif.getAttributeInt(
+//                    ExifInterface.TAG_ORIENTATION,
+//                    ExifInterface.ORIENTATION_NORMAL);
+//
+//            Log.v(TAG, "Orient: " + exifOrientation);
+//
+//            int rotate = 0;
+//
+//            switch (exifOrientation) {
+//                case ExifInterface.ORIENTATION_ROTATE_90:
+//                    rotate = 90;
+//                    break;
+//                case ExifInterface.ORIENTATION_ROTATE_180:
+//                    rotate = 180;
+//                    break;
+//                case ExifInterface.ORIENTATION_ROTATE_270:
+//                    rotate = 270;
+//                    break;
+//            }
+//
+//            Log.v(TAG, "Rotation: " + rotate);
+//
+//            if (rotate != 0) {
+//
+//                // Getting width & height of the given image.
+//                int w = bitmap.getWidth();
+//                int h = bitmap.getHeight();
+//
+//                // Setting pre rotate
+//                Matrix mtx = new Matrix();
+//                mtx.preRotate(rotate);
+//
+//                // Rotating Bitmap
+//                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+//            }
+//
+//            // Convert to ARGB_8888, required by tess
+//            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+//
+//        } catch (IOException e) {
+//            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+//        }
+//
+//        // _image.setImageBitmap( bitmap );
+//
+//        Log.v(TAG, "Before baseApi");
+//
+//        TessBaseAPI baseApi = new TessBaseAPI();
+//        baseApi.setDebug(true);
+//        baseApi.init(DATA_PATH, lang);
+//        baseApi.setImage(bitmap);
+//
+//        String recognizedText = baseApi.getUTF8Text();
+//
+//        baseApi.end();
+//
+//        // You now have the text in recognizedText var, you can do anything with it.
+//        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+//        // so that garbage doesn't make it to the display.
+//
+//        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+//
+//        if ( lang.equalsIgnoreCase("eng") ) {
+//            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+//        }
+//
+//        recognizedText = recognizedText.trim();
+//
+//        if ( recognizedText.length() != 0 ) {
+//            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+//            _field.setSelection(_field.getText().toString().length());
+//
+//        }
+//
+//
+//
+//        // Cycle done.
+//    }
 
 
     private TextView TextView(int _intID) {
@@ -303,5 +304,131 @@ public class OcrActivity extends Activity {
         tag.setLayoutParams(params);
         TextViewList.add(tag);
         return tag;
+    }
+
+    private class OCRTask extends AsyncTask<Void, Integer, String> {
+
+
+        ProgressDialog pd;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(OcrActivity.this);
+            pd.setTitle("Processing Image");
+            pd.setMessage("Extracting data out of the captured image.");
+            pd.setMax(100);
+            pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            pd.setCancelable(false);
+            pd.show();
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            _taken = true;
+            publishProgress(10);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 4;
+
+            Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+
+
+            try {
+                ExifInterface exif = new ExifInterface(_path);
+                int exifOrientation = exif.getAttributeInt(
+                        ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_NORMAL);
+
+                Log.v(TAG, "Orient: " + exifOrientation);
+
+                int rotate = 0;
+
+                switch (exifOrientation) {
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        rotate = 90;
+                        break;
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        rotate = 180;
+                        break;
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        rotate = 270;
+                        break;
+                }
+
+
+                Log.v(TAG, "Rotation: " + rotate);
+
+                if (rotate != 0) {
+
+                    // Getting width & height of the given image.
+                    int w = bitmap.getWidth();
+                    int h = bitmap.getHeight();
+
+                    // Setting pre rotate
+                    Matrix mtx = new Matrix();
+                    mtx.preRotate(rotate);
+
+                    // Rotating Bitmap
+                    bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+                }
+
+                // Convert to ARGB_8888, required by tess
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+            } catch (IOException e) {
+                Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+            }
+
+            // _image.setImageBitmap( bitmap );
+
+            Log.v(TAG, "Before baseApi");
+            publishProgress(30);
+            TessBaseAPI baseApi = new TessBaseAPI();
+            publishProgress(40);
+            baseApi.setDebug(true);
+            publishProgress(50);
+            baseApi.init(DATA_PATH, lang);
+            publishProgress(70);
+            baseApi.setImage(bitmap);
+            publishProgress(80);
+            String recognizedText = baseApi.getUTF8Text();
+            publishProgress(100);
+            baseApi.end();
+
+            // You now have the text in recognizedText var, you can do anything with it.
+            // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+            // so that garbage doesn't make it to the display.
+
+            Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+            if ( lang.equalsIgnoreCase("eng") ) {
+                recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+            }
+
+            recognizedText = recognizedText.trim();
+
+            return recognizedText;
+        }
+
+        protected void onPostExecute(String result) {
+            if (pd != null)
+            {
+                pd.dismiss();
+            }
+            if ( result.length() != 0 ) {
+                _field.setText(_field.getText().toString().length() == 0 ? result : _field.getText() + " " + result);
+                _field.setSelection(_field.getText().toString().length());
+
+            }
+        }
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+
+
+            super.onProgressUpdate(progress);
+            // Update the ProgressBar
+            pd.setProgress(progress[0]);
+
+
+
+        }
     }
 }
